520962b1b2ae7197a20732e7a56c702e127bd361
Jorge-Bill <jorge.mendesx@gmail.com>
2019-10-26 12:35:01 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import me from './img/avatar.jpeg';
+import './css/App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={me} className="App-logo" alt="Jorge Mendes" />
+        <h1>
+          Jorge Mendes
+        </h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         I'm a Web Developer
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Jorge-Bill"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Know me
         </a>
       </header>
     </div>
